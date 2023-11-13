ac7d9cef2f2bbec1caa9b8f877d35c761c669bf9
cmsfyan <150282761+cmsfyan@users.noreply.github.com>
2023-11-14 01:52:04 +0500

    Update img-girl.js

--- plugins/img-girl.js
+++ plugins/img-girl.js
@@ -5,12 +5,12 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 	//let girl = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/girl.json`)).data
 	let girl = await conn.getFile(global.API('fgmods', '/api/girl', { }, 'apikey'))
 	let img = girl.data
-   await conn.sendFile(m.chat, img, 'img.jpg', `✅ Tharki kun🤭`, m)
+   await conn.sendFile(m.chat, img, 'img.jpg', `✅ Tharki 🤭`, m)
    //conn.sendButton(m.chat, '✅ Result 🤭', igfg, img, [['▷▷ NEXT', `${usedPrefix + command}`]],m)
 }
 handler.help = ['girl']
 handler.tags = ['img']
-handler.command = ['girl', 'woman']
+handler.command = ['girl','saif', 'woman']
 handler.diamond = true
 
 export default handler
